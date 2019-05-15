9c603778bbfa9475508521ff1981ba1c818bf851
WizeHive <wizehive@Seans-MBP.lan>
2019-05-15 13:36:59 -0400

    testing heroku

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.use(express.static(__dirname +'/public'));
 // app.get('/', function (req, res) {
 //   // res.render('index', {});
 // });
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 app.post('/', (req, res) => {
   let data = req.body;
   let mealType = (req.body.selector);
